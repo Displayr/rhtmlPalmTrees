403177cbf413f396323be4400fd6bb3c686cdc4c
xtmwang <xtmwang@gmail.com>
2016-05-10 14:51:21 +1000

    documentation1

--- R/rhtmlPalmTrees.R
+++ R/rhtmlPalmTrees.R
@@ -18,6 +18,26 @@
 #' @param width
 #' @param height
 #'
+#' @examples
+#'
+#' data("CSDperceptions", package = "rhtmlPalmTrees")
+#'
+#' weights = rep(1,ncol(CSDperceptions))
+#' prefix = ""
+#' suffix = "%"
+#' rhtmlPalmTrees::PalmTrees(data = CSDperceptions,
+#'                          weights = weights,
+#'                          row.names = rownames(CSDperceptions),
+#'                          row.heading = names(dimnames(CSDperceptions))[1],
+#'                          col.names = colnames(CSDperceptions),
+#'                          col.heading = names(dimnames(CSDperceptions))[2],
+#'                          prefix = prefix,
+#'                          suffix = suffix,
+#'                          tooltips = TRUE,
+#'                          colors = qColors)
+#'
+#'
+#'
 #' @import htmlwidgets
 #' @export
 PalmTrees <- function(
